3bb9856181aede29b82466355f05051da67fc03d
daniyallko <38750820+daniyallko@users.noreply.github.com>
2020-10-20 20:19:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ if (process.env.HEROKU_APP_NAME) {
 				err ? rej(err) : res(text)
 			)
 		)
-	const APP_URL = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+	const APP_URL = `https://${process.env.HEROKU_APP_NAME}.cleverapps.io`
 	const preventIdling = () => {
 		request.post(
 			'http://localhost:6800/jsonrpc',
